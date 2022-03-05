0caed591a2a195f08589f56dd532df9560bb3921
MaansiBotke <maansibotke2298@gmail.com>
2022-03-05 22:21:26 +0530

    make it better

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  pic: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/maansi/image/upload/v1646476531/download_rr2cfj.png",
+  },
+  followers: [{ type: ObjectId, ref: "User" }],
+  following: [{ type: ObjectId, ref: "User" }],
+});
+
+mongoose.model("User", userSchema);
